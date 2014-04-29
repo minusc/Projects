9e352365541bf7d8f24e3a88466c18ecd05c260e
minusc <romina.caraba@gmail.com>
2014-04-29 23:28:24 +0300

    This is a homework project I'm working on.
    To do:
    	- Print to the screen the results from the functions 1,2 and 3.
    	- develop functions that sort the arrays in ascending and descending
    	- print the sum of the maximum numbers
    	- develop a function that switched the value from the first array to the second and the second to the first one
    	- insert case 4 and case 5 and develop the functions needed

--- homework.cpp
+++ homework.cpp
@@ -11,11 +11,12 @@
 #include<iostream>
 #include<string>
 
+
 using namespace std;
 
-void readArray(int tab[], int size);
-void printArray(int tab[], int size);
-void maxNumber(int tab1[],int tab2[], int size);
+void readArray(int array[], int size);
+void printArray(int array[], int size);
+void maxNumber(int array1[],int array2[], int size);
 
 int main()
 {
@@ -24,35 +25,86 @@ int main()
 	int size=5;
 	
 	int option;
-	char optionTwo[6];
+	int optionTwo;
 
 	int countProgram=0;
 
-	while(countProgram==0)
-	{
+//	while(countProgram==0)
+//	{
 		
 		cout<<"1. Read the values for the array\n"
 				<<"2.Print the first or second array\n"
-				<<"3.Print the the maxim sum out of the arrays\n"
+				<<"3.Print the sum of the maximum numbers in the arrays\n"
 				<<"4.Ascending sort 1 / Descending sort 2\n"
 				<<"5.Changing values between arrays\n";
 		cout<<"\n\nChoose a number that you want to use from the list above : ";
 		cin>>option;
 
 
-	}
+//	}
+	switch(option)
+	{
+		case 1:
+				 cout<<"Insert the values for the first array \n";
+             readArray(array_1,size);
+             cout<<"Insert the values for the second array : \n";
+             readArray(array_2,size);
+             break;
+		case 2:
+				 cout<<"Choose '1'  for printing the first table. Choose '2' for printing the second table: \n";
+             cin>>optionTwo;
 
+				if(optionTwo == '1')	
+				{
+               cout<<"First Array : \n";
+               printArray(array_1,size);
+              
+            }
+				if(optionTwo == '2')	
+				{
+               cout<<"Second Array : \n";
+               printArray(array_2,size);
+              
+            }
+				break;
+		case 3: maxNumber(array_1,array_2,size);
+              break;
+	}
 
 	return 0;
 }
 
-/*void readArray(int tab[], int size)
+void readArray(int array[], int size)
 {
-	
+	 int i;
+    for(i = 0; i < size; i++)
+    {
+        cout<< "Insert the array["<<i<<"] =";
+        cin>>array[i];
+    }
 
 
 }
-void printArray(int tab[], int size)
-void maxNumber(int tab1[],int tab2[], int size)
-*/
+void printArray(int array[], int size)
+{
+	int i;
+	cout<<"The numbers of the array are";
+	for(i = 0; i < size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+	
+}
+void maxNumber(int array1[],int array2[], int size)
+{
+	int i;
+	int array[10];
+	cout<<"The maximum value in the arrays are:";
+	for(i = 0; i < size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+}
+
 
